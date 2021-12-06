20df9023978d45165847fdadf5dbecd4bfd9bbc9
unknown <jen000@yandex.ru>
2021-12-06 23:53:45 +0300

    fragmets were made

--- app/src/main/java/example/jf/emotifications/MainActivity.kt
+++ app/src/main/java/example/jf/emotifications/MainActivity.kt
@@ -2,10 +2,40 @@ package example.jf.emotifications
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var bottomNavigationMenu: BottomNavigationView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        bottomNavigationMenu = findViewById(R.id.bottomNavigationView)
+        bottomNavigationMenu.setOnItemSelectedListener { item ->
+            var fragment: Fragment? = null
+            when (item.itemId) {
+                R.id.calendar_page_fragment -> {
+                    fragment = CalendarFragment()
+                }
+                R.id.feed_page_fragment -> {
+                    fragment = FeedFragment()
+                }
+                R.id.profile_page_fragment -> {
+                    fragment = ProfileFragment()
+                }
+            }
+            replaceFragment(fragment!!)
+
+            true
+        }
+        bottomNavigationMenu.selectedItemId = R.id.feed_page_fragment
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.mainFragmentContainerView, fragment)
+            .addToBackStack(fragment.tag)
+            .commit()
     }
 }
